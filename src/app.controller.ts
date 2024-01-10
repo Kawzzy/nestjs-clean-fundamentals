f0d92a220295cecceacc8da5c8e2d0791fd3de89
Augusto Kalahary <AuuKalaharyKW@gmail.com>
2024-01-10 15:48:48 -0300

    Adding class for database connection

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-	constructor(private readonly appService: AppService) {}
+	constructor(private readonly appService: AppService, private readonly prismaConnection: PrismaService) {}
 
   @Get()
 	getHello(): string {
