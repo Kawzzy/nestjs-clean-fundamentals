16141e0b08b3bf9821ac4a286c04727767083b09
Augusto Kalahary <AuuKalaharyKW@gmail.com>
2024-01-13 14:15:58 -0300

    Repositories initial config

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,8 +1,30 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-repository';
+import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions-repository';
+import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository';
+import { PrismaQuestionCommentRepository } from './prisma/repositories/prisma-question-comments-repository';
+import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answer-attachments-repository';
+import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-question-attachments-repository';
 
 @Module({
-	providers: [PrismaService],
-	exports: [PrismaService]
+	providers: [
+		PrismaService,
+		PrismaAnswersRepository,
+		PrismaQuestionsRepository,
+		PrismaAnswerCommentsRepository,
+		PrismaQuestionCommentRepository,
+		PrismaAnswerAttachmentsRepository,
+		PrismaQuestionAttachmentsRepository
+	],
+	exports: [
+		PrismaService,
+		PrismaAnswersRepository,
+		PrismaQuestionsRepository,
+		PrismaAnswerCommentsRepository,
+		PrismaQuestionCommentRepository,
+		PrismaAnswerAttachmentsRepository,
+		PrismaQuestionAttachmentsRepository
+	]
 })
 export class DatabaseModule {}
