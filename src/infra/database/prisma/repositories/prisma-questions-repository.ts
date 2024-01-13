4425c6c512bc0b81b9501e5f0639ee4a473f8c02
Augusto Kalahary <AuuKalaharyKW@gmail.com>
2024-01-13 14:46:57 -0300

    Creating a Mapper and example of usage

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -1,31 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
 import { Question } from '@/domain/forum/enterprise/entities/question';
 import { PaginationParams } from '@/core/repositories/pagination-params';
+import { PrismaQuestionMapper } from '../mappers/prisma-question.mapper';
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository';
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionsRepository {
-	findById(id: string): Promise<Question | null> {
+	
+	constructor(private prismaConnection: PrismaService) {}
+
+	async findById(id: string): Promise<Question | null> {
+		const question = await this.prismaConnection.question.findUnique({
+			where: {
+				id
+			}
+		});
+
+		return question ? PrismaQuestionMapper.toDomain(question) : null;
+	}
+
+	async findBySlug(slug: string): Promise<Question | null> {
 		throw new Error('Method not implemented.');
 	}
 
-	findBySlug(slug: string): Promise<Question | null> {
+	async findManyRecent(params: PaginationParams): Promise<Question[]> {
 		throw new Error('Method not implemented.');
 	}
 
-	findManyRecent(params: PaginationParams): Promise<Question[]> {
+	async save(question: Question): Promise<void> {
 		throw new Error('Method not implemented.');
 	}
 
-	save(question: Question): Promise<void> {
+	async create(question: Question): Promise<void> {
 		throw new Error('Method not implemented.');
 	}
 
-	create(question: Question): Promise<void> {
-		throw new Error('Method not implemented.');
-	}
-
-	delete(question: Question): Promise<void> {
+	async delete(question: Question): Promise<void> {
 		throw new Error('Method not implemented.');
 	}
 }
