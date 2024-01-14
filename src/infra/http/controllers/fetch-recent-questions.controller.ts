5ee356307a077a739ccd4dee01f1ded3ca391b04
Augusto Kalahary <AuuKalaharyKW@gmail.com>
2024-01-14 13:54:06 -0300

    Completing communication between layers - fetch recent questions

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 import { AuthGuard } from '@nestjs/passport';
-import { PrismaService } from '@/infra/database/prisma/prisma.service';
 import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation.pipe';
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions';
 
 const pageValidationParamSchema = z.string().optional().default('1').transform(Number).pipe(z.number().min(1));
 
@@ -13,18 +13,13 @@ type PageValidationParamSchema = z.infer<typeof pageValidationParamSchema>
 @Controller('/questions')
 @UseGuards(AuthGuard('jwt'))
 export class FetchRecentQuestionsController {
-	constructor(private prismaConnection: PrismaService) {}
+	constructor(private fetchRecentQuestions: FetchRecentQuestionsUseCase) {}
 
     @Get()
 	async handle(@Query('page', queryValidationPipe) page: PageValidationParamSchema) {
-		const perPage = 20;
 
-		const questions = await this.prismaConnection.question.findMany({
-			take: perPage,
-			skip: (page - 1) * perPage,
-			orderBy: {
-				createdAt: 'desc'
-			}
+		const questions = await this.fetchRecentQuestions.execute({
+			page
 		});
 
 		return { questions };
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -7,6 +7,7 @@ import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/crea
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller';
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository';
 import { PrismaQuestionsRepository } from '../database/prisma/repositories/prisma-questions-repository';
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions';
 
 @Module({
 	imports: [DatabaseModule],
@@ -21,6 +22,13 @@ import { PrismaQuestionsRepository } from '../database/prisma/repositories/prism
 			provide: CreateQuestionUseCase,
 			useFactory: (questionsRepository: QuestionsRepository) => {
 				return new CreateQuestionUseCase(questionsRepository);
+			},
+			inject: [PrismaQuestionsRepository]
+		},
+		{
+			provide: FetchRecentQuestionsUseCase,
+			useFactory: (questionsRepository: QuestionsRepository) => {
+				return new FetchRecentQuestionsUseCase(questionsRepository);
 			},
 			inject: [PrismaQuestionsRepository]
 		}
