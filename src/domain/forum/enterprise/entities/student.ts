27a80e1a033ccc79ed13f9d6ef151912a04ca0c9
Augusto Kalahary <AuuKalaharyKW@gmail.com>
2024-01-14 16:18:52 -0300

    Register and Authenticate Students' use case

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -3,12 +3,27 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 
 interface StudentProps {
   name: string
+  email: string
+  password: string
 }
 
 export class Student extends Entity<StudentProps> {
+	
 	static create(props: StudentProps, id?: UniqueEntityID) {
 		const student = new Student(props, id);
 
 		return student;
+	}
+
+	get name() {
+		return this.props.name;
+	}
+
+	get email() {
+		return this.props.email;
+	}
+
+	get password() {
+		return this.props.password;
 	}
 }
