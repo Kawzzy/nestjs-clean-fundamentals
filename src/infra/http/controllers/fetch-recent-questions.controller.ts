9b70609bf46b762ed2e8d3ae90a2075da6d152a5
Augusto Kalahary <AuuKalaharyKW@gmail.com>
2024-01-14 14:21:46 -0300

    Creating a Presenter and example of usage

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 import { AuthGuard } from '@nestjs/passport';
 import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation.pipe';
+import { HttpQuestionPresenter } from '../presenters/http-question-presenter';
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions';
 
 const pageValidationParamSchema = z.string().optional().default('1').transform(Number).pipe(z.number().min(1));
@@ -18,10 +19,16 @@ export class FetchRecentQuestionsController {
     @Get()
 	async handle(@Query('page', queryValidationPipe) page: PageValidationParamSchema) {
 
-		const questions = await this.fetchRecentQuestions.execute({
+		const result = await this.fetchRecentQuestions.execute({
 			page
 		});
 
-		return { questions };
+		if (result.isLeft()) {
+			throw new Error();
+		}
+
+		const questions = result.value.questions;
+
+		return { questions: questions.map(HttpQuestionPresenter.toHTTP) };
 	}
 }
