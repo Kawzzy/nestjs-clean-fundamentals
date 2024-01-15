5049dea8005e6c60d015a884cf754d539d362ba1
Augusto Kalahary <AuuKalaharyKW@gmail.com>
2024-01-14 21:15:09 -0300

    Refactoring RegisterController

--- src/infra/http/controllers/create-account.controller.ts
+++ src/infra/http/controllers/create-account.controller.ts
@@ -1,8 +1,7 @@
 import { z } from 'zod';
-import { hash } from 'bcryptjs';
-import { PrismaService } from '@/infra/database/prisma/prisma.service';
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation.pipe';
-import { Body, ConflictException, Controller, Post, UsePipes } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes } from '@nestjs/common';
+import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student';
 
 const createAccountBodySchema = z.object({
 	name: z.string(),
@@ -14,31 +13,21 @@ type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
 
 @Controller('/accounts')
 export class CreateAccountController {
-	constructor(private prismaConnection: PrismaService) {}
+	constructor(private registerStudent: RegisterStudentUseCase) {}
 
     @Post()
 	@UsePipes(new ZodValidationPipe(createAccountBodySchema))
 	async handle(@Body() body: CreateAccountBodySchema) {
 		const { name, email, password } = body;
 
-		const existingUser = await this.prismaConnection.user.findUnique({
-			where: {
-				email
-			}
+		const result = await this.registerStudent.execute({
+			name,
+			email,
+			password
 		});
 
-		if (existingUser) {
-			throw new ConflictException('User already exists!');
+		if (result.isLeft()) {
+			throw new Error();
 		}
-
-		const hashedPassword = await hash(password, 8);
-
-		await this.prismaConnection.user.create({
-			data: {
-				name,
-				email,
-				password: hashedPassword
-			}
-		});
 	}
 }
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -8,7 +8,9 @@ import { AuthenticateController } from './controllers/authenticate.controller';
 import { CreateAccountController } from './controllers/create-account.controller';
 import { CreateQuestionController } from './controllers/create-question.controller';
 import { HashComparer } from '@/domain/forum/application/cryptography/hash-comparer';
+import { HashGenerator } from '@/domain/forum/application/cryptography/hash-generator';
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question';
+import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student';
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller';
 import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository';
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository';
@@ -49,6 +51,13 @@ import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-case
 				return new AuthenticateStudentUseCase(studentsRepository, hashComparer, encrypter);
 			},
 			inject: [PrismaStudentsRepository, BcryptHasher, JwtEncrypter]
+		},
+		{
+			provide: RegisterStudentUseCase,
+			useFactory: (studentsRepository: StudentsRepository, hashGenerator: HashGenerator) => {
+				return new RegisterStudentUseCase(studentsRepository, hashGenerator);
+			},
+			inject: [PrismaStudentsRepository, BcryptHasher]
 		}
 	]
 })
