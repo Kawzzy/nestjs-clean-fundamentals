98ffb7ffac72f9441f3cee7303e7963189604236
Augusto Kalahary <AuuKalaharyKW@gmail.com>
2024-01-15 13:27:06 -0300

    Creating Mappers

---
+++ src/infra/database/prisma/mappers/prisma-answer-attachment.mapper.ts
@@ -0,0 +1,17 @@
+import { Attachment as PrismaComment } from '@prisma/client';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { AnswerAttachment } from '@/domain/forum/enterprise/entities/answer-attachment';
+
+export class PrismaAnswerAttachmentMapper {
+
+	static toDomain(raw: PrismaComment): AnswerAttachment {
+		if (!raw.answerId) {
+			throw new Error('Invalid attachment type.');
+		}
+
+		return AnswerAttachment.create({
+			attachmentId: new UniqueEntityID(raw.id),
+			answerId: new UniqueEntityID(raw.answerId)
+		}, new UniqueEntityID(raw.id));
+	}
+}
