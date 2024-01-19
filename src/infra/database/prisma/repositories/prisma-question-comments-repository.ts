5db0b6b4f8415f8204a4faaebf8cbf87823b731c
Augusto Kalahary <AuuKalaharyKW@gmail.com>
2024-01-19 15:03:43 -0300

    Refactoring QuestionComments' prisma's repository and Mapper

--- src/infra/database/prisma/repositories/prisma-question-comments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-question-comments-repository.ts
@@ -3,6 +3,8 @@ import { PrismaService } from '../prisma.service';
 import { PaginationParams } from '@/core/repositories/pagination-params';
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment';
 import { PrismaQuestionCommentMapper } from '../mappers/prisma-question-comment.mapper';
+import { PrismaCommentWithAuthorMapper } from '../mappers/prisma-comment-with-author.mapper';
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author';
 import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository';
 
 @Injectable()
@@ -33,6 +35,24 @@ export class PrismaQuestionCommentRepository implements QuestionCommentsReposito
 		});
 
 		return questionComments.map(PrismaQuestionCommentMapper.toDomain);
+	}
+    
+	async findManyByQuestionIdWithAuthor(questionId: string, { page }: PaginationParams): Promise<CommentWithAuthor[]> {
+		const comments = await this.prismaConnection.comment.findMany({
+			where: {
+				questionId
+			},
+			include: {
+				author: true
+			},
+			orderBy: {
+				createdAt: 'desc'
+			},
+			take: 20,
+			skip: (page - 1) * 20
+		});
+
+		return comments.map(PrismaCommentWithAuthorMapper.toDomain);
 	}
     
 	async create(questionComment: QuestionComment): Promise<void> {
